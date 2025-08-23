834a65a78dcc5bf1e59c543ccc3afe4a7201637a
jmendozarv <52211870+jmendozarv@users.noreply.github.com>
2025-08-23 16:41:59 -0500

    contact proyect with quarkus

---
+++ contact-with-quarkus/src/test/java/org/quarkus/apudigital/contactIT.java
@@ -0,0 +1,8 @@
+package org.quarkus.apudigital;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class contactIT extends contactTest {
+    // Execute the same tests but in packaged mode.
+}
